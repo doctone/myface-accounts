d51cffdc149d7946e9f55a59ae166a621ad4ce2c
Sam James <samjojames@gmail.com>
2022-03-09 16:38:24 +0000

    Remove unnecessary imports

--- myface-ui/src/Pages/Feed/Feed.tsx
+++ myface-ui/src/Pages/Feed/Feed.tsx
@@ -1,11 +1,10 @@
-﻿import React, {useContext} from 'react';
+﻿import React from 'react';
 import {Page} from "../Page/Page";
 import {fetchPosts} from "../../Api/apiClient";
 import {PostCard} from "../../Components/PostCard/PostCard";
 import {InfiniteList} from "../../Components/InfititeList/InfiniteList";
 import {Link} from "react-router-dom";
 import './Feed.scss';
-import { LoginContext } from '../../Components/LoginManager/LoginManager';
 
 export function Feed(): JSX.Element {
     
